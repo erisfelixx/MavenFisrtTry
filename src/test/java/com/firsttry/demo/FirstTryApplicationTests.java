492671b7915063d6aac0a1024892c920b49c8983
anya <anna.sovgirya@gmail.com>
2025-08-16 22:49:26 +0300

    Initial commit

---
+++ src/test/java/com/firsttry/demo/FirstTryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firsttry.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstTryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
